509df2da8603b3bf20f0a3b1481298c55defff02
KazimierzKochan <kazimierzkochan@wp.pl>
2022-04-08 18:30:03 +0200

    Finished lab1 ex1

--- lab1/zad1/client.c
+++ lab1/zad1/client.c
@@ -14,25 +14,58 @@ Warsaw Univeristy of Technology
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define MAX_LINE 1024
 #define SERVER "192.168.56.110"
 #define SERVER_PORT "3792"
 
-int main(){
-	//time
-	time_t rawtime;
-	struct tm * timeinfo;
-	unsigned char stringtime[MAX_LINE];
+int main(int argc, char *argv[]){
+
+	//assess arguments
+	bool done = false;
+	double uptime;
+	if(argc <= 2){
+		//if time given
+		if(argc == 2){
+			if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0){
+				printf("Syntax: %s [uptime (0=infinity)]\n", argv[0]);
+				exit(0);
+			}else{
+				uptime = atof(argv[1]);
+			}
+		}else{
+			uptime = 0;
+		}
+	}else{
+		printf("Syntax: ./%s [uptime (0=infinity)]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	//time info
+	time_t raw_time;
+	struct tm * time_info;
+	unsigned char string_time[MAX_LINE];
+
+	//uptime
+	time_t start_time;
+	double time_elapsed;
+	time(&start_time);
 
 	int s; //socket
-	struct addrinfo h;
-	struct addrinfo *r;
+	struct addrinfo h; //hints
+	struct addrinfo *r; //res
 
+	//get address info
 	memset(&h, 0, sizeof(struct addrinfo));
 	getaddrinfo(SERVER, SERVER_PORT, &h, &r);
 
 	printf("PSIR 22L Lab1, exercise 1: Simple TCP client\n");
+	if(uptime == 0){
+		printf("Infinite loop\n");
+	}else{
+		printf("Planned uptime: %.1f seconds\n", uptime);
+	}
 
 	//create a socket
 	s=socket(r->ai_family, r->ai_socktype, r->ai_protocol);
@@ -46,26 +79,38 @@ int main(){
 		fprintf(stderr, "ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__-1);
 		exit(EXIT_FAILURE);
 	}
+
 	//main loop
-	for(;;){
+	while(!done){
 		//create current time string
-		time(&rawtime);
-		timeinfo = localtime(&rawtime);
-		snprintf(stringtime, MAX_LINE, asctime(timeinfo));
+		time(&raw_time);
+		time_info = localtime(&raw_time);
+		snprintf(string_time, MAX_LINE, asctime(time_info));
 
 		//send packet
-		printf("Sending current time: %s", stringtime);
-		if(send(s, stringtime, strlen(stringtime), 0) < 0){
+		printf("Sending current time: %s", string_time);
+		if(send(s, string_time, strlen(string_time), 0) < 0){
 			fprintf(stderr, "ERROR while trying to send packet (%s:%d)\n", __FILE__, __LINE__-1);
 			exit(EXIT_FAILURE);
 		}
+
 		//sleep for 1030 ms
+		//no need to use non-blocking methods
 		usleep(1030000);
+
+		//check if client should end its job
+		time_elapsed = difftime(raw_time, start_time);
+		if(time_elapsed >= uptime && uptime != 0){
+			done = true;
+			printf("Client has worked for %.1f seconds\n", time_elapsed);
+		}
 	}
 
+	printf("Quitting...\n");
 	//clean up
 	freeaddrinfo(r);
 	close(s);
 
 	return 0;
 }
+
--- lab1/zad1/server.c
+++ lab1/zad1/server.c
@@ -13,16 +13,45 @@ Warsaw Univeristy of Technology
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
 
 #define MAX_BUF 128
 #define SERVER_PORT "3792"
 
-int main(){
+int main(int argc, char *argv[]){
+
+	//assess arguments
+	bool done = false;
+	double uptime;
+	if(argc <= 2){
+		//if time given
+		if(argc == 2){
+			if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0){
+				printf("Syntax: %s [uptime (0=infinity)]\n", argv[0]);
+				exit(0);
+			}else{
+				uptime = atof(argv[1]);
+			}
+		}else{
+			uptime = 0;
+		}
+	}else{
+		printf("Syntax: ./%s [uptime (0=infinity)]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
 	int s, new_s; //sockets
-	struct addrinfo h, *r;
-	unsigned char mip_str[INET_ADDRSTRLEN];
+	struct addrinfo h, *r; //hints, res
+	unsigned char mip_str[INET_ADDRSTRLEN]; //ip address
 	char message[MAX_BUF];
 
+	//uptime
+	time_t start_time;
+	double time_elapsed;
+	time(&start_time);
+
+	//set addrinfo
 	memset(&h, 0, sizeof(struct addrinfo));
 	h.ai_family=PF_INET;
 	h.ai_socktype=SOCK_STREAM;
@@ -31,6 +60,11 @@ int main(){
 	getaddrinfo(NULL, SERVER_PORT, &h, &r);
 
 	printf("PSIR 22L Lab1, exercise 1: Simple TCP server\n");
+	if(uptime == 0){
+		printf("Infinite loop\n");
+	}else{
+		printf("Planned uptime: %.1f seconds\n", uptime);
+	}
 
 	//create a socket
 	s=socket(r->ai_family, r->ai_socktype, r->ai_protocol);
@@ -38,11 +72,13 @@ int main(){
 		fprintf(stderr, "ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__-2);
 		exit(EXIT_FAILURE);
 	}
+
 	//bind
 	if(bind(s, r->ai_addr, r->ai_addrlen)!=0){
 		fprintf(stderr, "ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__-1);
 		exit(EXIT_FAILURE);
 	}
+
 	//listen for the client
 	printf("Listening for the client\n");
 	if(listen(s, 1)!=0){
@@ -58,8 +94,9 @@ int main(){
 		fprintf(stderr, "ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__-1);
 	if(inet_ntop(AF_INET, &(their_addr.sin_addr), mip_str, INET_ADDRSTRLEN)!=NULL)
 		printf("Client with IP: %s has connected. New sock desc.: %d\n", mip_str, new_s);
+
 	//main loop
-	for(;;){
+	while(!done){
 		//receive data from client
 		int result=recv(new_s, message, MAX_BUF, 0);
 		if(result==0){
@@ -73,11 +110,20 @@ int main(){
 		//end message with a null char
 		message[result]='\0';
 		printf("Client sent data: %s", message);
+
+		//check if server should end its job
+		time_elapsed = difftime(time(NULL), start_time);
+		if(time_elapsed >= uptime && uptime != 0){
+			done = true;
+		}
 	}
 
+	printf("Server has worked for %.1f seconds\n", time_elapsed);
+	printf("Quitting...\n");
 	//clean up
 	close(s);
 	freeaddrinfo(r);
 
 	return 0;
 }
+
